3af8c1c6734b45c73c19720c4eaa138126d251c1
viktor723 <viktor.ramic.simic@abbgymnasiet.se>
2022-09-02 14:30:02 +0200

    header och ino´fo page

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,32 @@
+<template>
+<header>
+<nav class="container flex justify-between px-4 py-8 mx-auto bg-white">
+  <div>
+    <h3 class="text-2xl font-medium text-blue-500">LOGO</h3>
+  </div>
+  <div class="hidden space-x-8 lg:flex text-white-500">
+    <a href="">Home</a>
+    <a href="">About Us</a>
+    <a href="">Blogs</a>
+    <a href="">Our Team</a>
+    <a href="">Contact Us</a>
+  </div>
+  <div class="flex lg:hidden">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-6 h-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M4 6h16M4 12h16M4 18h16"
+      />
+    </svg>
+  </div>
+</nav>
+</header>
+</template>
---
+++ pages/information.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="om-oss">
+
+    <h1>Information</h1>
+
+    </div>
+</template>
+
+<style>
+.om-oss {
+display: flex;
+height: 600px;
+background-color:white ;
+width: 500px;
+position: absolute;
+top: 100px;
+left: 510px;
+justify-content: center;
+border-radius: 30px;
+
+
+}
+h1{
+    font-size: 30px;
+}
+
+</style>
