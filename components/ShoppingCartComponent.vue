39f80d88943326918c6a38361b7b7e9a53bd9d60
abbolltho <olle.thor@abbgymnasiet.se>
2022-09-07 13:26:09 +0200

    shopping cart

---
+++ components/ShoppingCartComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="h-full w-full">
+        <div class="cart">
+            Shopping cart
+        </div>
+    </div>
+</template>
+
+<style>
+.cart{
+    background-color: blueviolet;
+    bottom:0;
+    position: absolute;
+}
+</style>
