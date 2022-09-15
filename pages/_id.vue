66d1a05b6b9b67ddfa6bca383f875c92aded305c
viktor723 <viktor.ramic.simic@abbgymnasiet.se>
2022-09-15 14:13:21 +0200

    book id

--- pages/_id.vue
+++ pages/_id.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    <h1>{{ post.title }}</h1>
-    <p>{{ post.description }}</p>
+    {{bla.books[1].author}}
   </div>
 </template>
 
 <script>
-  export default {
+export default {
     async asyncData({ params, $axios }) {
-      const post = await $axios.$get(`https://api.nuxtjs.dev/posts/1`)
-      return { post }
+      const post = await $axios.$get(`https://api.nuxtjs.dev/posts/1}`)
+      console.log(params.id);
+      const bla = require("../db.json");
+      return { bla, params }
     }
   }
 </script>
